d80bdb43319b40e7fd6fd38e9e154b3ef96a4282
Vega <vegafernando881@gmail.com>
2024-10-30 17:52:10 -0600

    Gun Catalogue Completed

---
+++ src/main/webapp/scripts/utils/cardData.js
@@ -0,0 +1,87 @@
+// Data Cards Customer
+const customerCards = [
+    {
+        imgSrc: "https://images.unsplash.com/photo-1649424219416-15e139f13cc6?q=80&w=1974&auto=format&fit=crop",
+        title: "Registrar Cliente",
+        linkHref: "./register_customer.html",
+        linkText: "Registrar"
+    },
+    {
+        imgSrc: "https://images.unsplash.com/photo-1600469984476-c713650f1b1b?q=80&w=2070&auto=format&fit=crop",
+        title: "Modificar Cliente",
+        linkHref: "./search_customer_form.html",
+        linkText: "Actualizar"
+    },
+    {
+        imgSrc: "https://images.unsplash.com/photo-1616499370260-485b3e5ed653?q=80&w=2070&auto=format&fit=crop",
+        title: "Buscar Cliente",
+        linkHref: "./search_customer_form.html",
+        linkText: "Consultar"
+    }
+];
+
+// Data Cards Sales
+const salesCards = [
+    {
+        imgSrc: "https://images.unsplash.com/photo-1556740772-1a741367b93e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Registrar Venta",
+        linkHref: "#",
+        linkText: "Registrar"
+    },
+    {
+        imgSrc: "https://images.unsplash.com/photo-1593437955835-fc8c51725430?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Buscar Venta",
+        linkHref: "#",
+        linkText: "Consultar"
+    }
+];
+
+// Data Cards Catalogue 
+const catalogueCards = [
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/03/05/06/rostmartin-rm1c-black.1708549237676-500x333.png",
+        title: "Rost Martin RM1C 9mm - Black",
+        price: "438.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/02/26/23/rostmartin-rm1c-fde.1708547953944-600x400.png",
+        title: "Rost Martin RM1C 9mm - FDE",
+        price: "438.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/02/27/00/rostmartin-rm1c-stonegrey.1708547760850-600x400.png",
+        title: "Rost Martin RM1C 9mm - Stone Grey",
+        price: "438.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/02/22/12/206538210-400x312.png",
+        title: "Beretta Bpcc",
+        price: "313.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/02/22/13/206543706-400x300.png",
+        title: "Bersa Thunder",
+        price: "293.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/03/05/08/150142289-400x273.png",
+        title: "Bond Arms Century",
+        price: "526.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/02/27/02/150146732-400x108.png",
+        title: "Aero Precision Ar - 15",
+        price: "1, 355.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/01/27/11/206539659-400x119.png",
+        title: "Ati Mil - Sport",
+        price: "491.99"
+    },
+    {
+        imgSrc: "https://gatguns.com/wp-content/uploads/2024/02/21/10/211274760-400x66.png",
+        title: "Bergara Rifles B - 14s",
+        price: "2, 075.99"
+    }
+
+];
