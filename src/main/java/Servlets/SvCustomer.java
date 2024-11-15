a402fbae23147dd1554d4b7edb17d2f1e1fa09e7
Vega <vegafernando881@gmail.com>
2024-11-14 23:35:16 -0600

    Project Done (Algo Asi)

--- src/main/java/Servlets/SvCustomer.java
+++ src/main/java/Servlets/SvCustomer.java
@@ -4,7 +4,7 @@ import DAO.CustomerDAO;
 import DAO.UserDAO;
 import EntityClass.Customer;
 import EntityClass.User;
-import jakarta.servlet.RequestDispatcher;
+import ValidSignature.VerFir;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
@@ -15,8 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 @WebServlet(name = "SvCustomer", urlPatterns = {"/SvCustomer"})
 @MultipartConfig
@@ -82,57 +81,59 @@ public class SvCustomer extends HttpServlet {
             }
         } catch (SQLException ex) {
             Logger.getLogger(SvCustomer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(SvCustomer.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
     
     // Create Customer Using Form Fields
-    private void createCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+    private void createCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException, Exception {
         // Form Fields
-        String rfc_customer = request.getParameter("rfc");
-        String name_customer = request.getParameter("full_name");
-        String curp_customer = request.getParameter("curp");
-        String date_birth_customer = request.getParameter("birth_date");
-        String nacionality_customer = request.getParameter("nacionality");
-        String adress_customer = request.getParameter("adress");
-        String civil_state_customer = request.getParameter("civil_state");
-        String profession_customer = request.getParameter("ocupation");
-        String degree_study_customer = request.getParameter("study_degree");
-        
-        // Conversion of Variables
-        LocalDate date_birth_conversion = LocalDate.parse(date_birth_customer);
+            String rfc_customer = request.getParameter("rfc");
+            String name_customer = request.getParameter("full_name");
+            String curp_customer = request.getParameter("curp");
+            String date_birth_customer = request.getParameter("birth_date");
+            String nacionality_customer = request.getParameter("nacionality");
+            String adress_customer = request.getParameter("adress");
+            String civil_state_customer = request.getParameter("civil_state");
+            String profession_customer = request.getParameter("ocupation");
+            String degree_study_customer = request.getParameter("study_degree");
 
-        // Create User Using RFC For Username and Password
-        User user = new User(0, rfc_customer, rfc_customer, 2);
-        UserDAO userDAO = new UserDAO();
-        int id_user = userDAO.createUser(user);
+            // Conversion of Variables
+            LocalDate date_birth_conversion = LocalDate.parse(date_birth_customer);
 
-        // Create Customer Using the ID Returned
-        Customer customer = new Customer(rfc_customer, id_user, name_customer, curp_customer, date_birth_conversion, nacionality_customer, adress_customer, civil_state_customer, profession_customer, degree_study_customer);
-        CustomerDAO customerDAO = new CustomerDAO();
-        
-        // Set Attributes and Pass to Info Page
-        HttpSession session = request.getSession();
-        
-        try {
-            // Call Create Method in CustomerDAO
-            boolean isCreated = customerDAO.createCustomer(customer);
-                
-            // Set Appropriate Message Based on Result
-            if (isCreated) {
-                session.setAttribute("message", "Registro Exitoso");
-            } else {
-                session.setAttribute("message", "Error en el Registro");
+            // Create User Using RFC For Username and Password
+            User user = new User(0, rfc_customer, rfc_customer, 2);
+            UserDAO userDAO = new UserDAO();
+            int id_user = userDAO.createUser(user);
+
+            // Create Customer Using the ID Returned
+            Customer customer = new Customer(rfc_customer, id_user, name_customer, curp_customer, date_birth_conversion, nacionality_customer, adress_customer, civil_state_customer, profession_customer, degree_study_customer);
+            CustomerDAO customerDAO = new CustomerDAO();
+
+            // Set Attributes and Pass to Info Page
+            HttpSession session = request.getSession();
+            
+            try {
+                // Call Create Method in CustomerDAO
+                boolean isCreated = customerDAO.createCustomer(customer);
+
+                // Set Appropriate Message Based on Result
+                if (isCreated) {
+                    session.setAttribute("message", "Registro Exitoso");
+                } else {
+                    session.setAttribute("message", "Error en el Registro");
+                }
+
+                // Redirect to the Other Servlet
+                response.sendRedirect("system_pages/customer/register_customer.jsp");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                session.setAttribute("message", "Error al Procesar la Solicitud");
+                response.sendRedirect("system_pages/customer/register_customer.jsp");
             }
-            
-            // Redirect to the Other Servlet
-            response.sendRedirect("system_pages/customer/register_customer.jsp");
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-            session.setAttribute("message", "Error al Procesar la Solicitud");
-            response.sendRedirect("system_pages/customer/register_customer.jsp");
-        }
     }
     
     // Search Customer By RFC 
